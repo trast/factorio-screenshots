398a7a47826a67003600fc26068e36fd797d2036
Thomas Rast <tr@thomasrast.ch>
2016-03-19 18:52:59 +0100

    Revamp with parallel processing and new zooms

--- shrink.py
+++ shrink.py
@@ -2,12 +2,14 @@
 
 from __future__ import division
 
+import multiprocessing
 import os
-import sys
 import cStringIO as StringIO
+import sys
+
 from PIL import Image
 
-FULL_ZOOM = 9
+FULL_ZOOM = 11
 
 def puterr(x):
     sys.stderr.write(x)
@@ -15,38 +17,46 @@ def puterr(x):
 
 def save(im, z, x, y):
     ox = x % (2**z)
-    oy = y
+    oy = y % (2**z)
     try:
         os.makedirs('%d/%d' % (z, ox))
     except OSError:
         pass
-    puterr('%d/%d/%d.jpg\n' % (z, ox, oy))
     im.save('%d/%d/%d.jpg' % (z, ox, oy))
 
-def do_one_shrink(z, x, y):
+def do_one_shrink((z, x, y)):
     im = Image.new('RGB', (512, 512))
     for i in range(2):
         for j in range(2):
             try:
                 im.paste(Image.open('%d/%d/%d.jpg' % (z+1, 2*x+i, 2*y+j)), (i*256, j*256))
             except IOError:
-                pass  # meh
+                pass  # meh, remains black
     save(im.resize((256, 256), Image.BICUBIC), z, x, y)
 
-def do_shrink(z):
+def do_shrink(z, pool=None):
     xs = [int(x) for x in os.listdir(str(z+1))]
+    done = set()
+    shrinks_to_do = []
     for x in xs:
-        if x % 2:
-            continue
+        x1 = x - x%2
         ys = [int(os.path.splitext(y)[0]) for y in os.listdir('%d/%d' % (z+1, x))]
         for y in ys:
-            if y % 2:
+            y1 = y - y%2
+            if (x1, y1) in done:
                 continue
-            do_one_shrink(z, x//2, y//2)
+            done.add((x1, y1))
+            shrinks_to_do.append((z, x1//2, y1//2))
+    if pool:
+        pool.map(do_one_shrink, shrinks_to_do)
+    else:
+        map(do_one_shrink, shrinks_to_do)
 
 def main():
+    pool = multiprocessing.Pool(12)
     for z in range(FULL_ZOOM-1, 0, -1):
-        do_shrink(z)
+        puterr("%d\n" % z)
+        do_shrink(z, pool)
 
 if __name__ == '__main__':
     main()
--- slice.py
+++ slice.py
@@ -7,36 +7,33 @@ import sys
 import cStringIO as StringIO
 from PIL import Image
 
-FULL_ZOOM = 9
+FULL_ZOOM = 11
 
 def puterr(x):
     sys.stderr.write(x)
     sys.stderr.flush()
 
 def save(im, z, x, y):
-    ox = x % (2**z)
-    if ox < 0:
-        ox += 2**z
-    oy = y + 2**(z-1)
+    # logical 0/0 is at (2**(z-1), 2**(z-1))
+    ox = (x + 2**(z-1)) % (2**z)
+    oy = (y + 2**(z-1)) % (2**z)
     assert 0 <= ox < 2**z, "ox=%d out of range" % ox
     assert 0 <= oy < 2**z, "oy=%d out of range" % oy
     try:
         os.makedirs('%d/%d' % (z, ox))
     except OSError:
         pass
-    puterr('%d/%d/%d.jpg\n' % (z, ox, oy))
     im.save('%d/%d/%d.jpg' % (z, ox, oy))
 
 def do_slice(f):
     _, sx, sy = os.path.splitext(f)[0].split('_')
-    # center of image is the x/y coordinate in filename
-    # 32x32 in every image -> 8x8 in every tile
-    x = (int(sx) - 16) // 8
-    y = (int(sy) - 16) // 8
+    # input is in chunk coordinates, which we slice into 2x2 tiles
+    x = int(sx) * 2
+    y = int(sy) * 2
     im = Image.open(f)
-    for i in range(4):
-        for j in range(4):
-            save(im.crop((i*256, j*256, (i+1)*256, (j+1)*256)), FULL_ZOOM, x+i, y+j)
+    for i in range(2):
+        for j in range(2):
+            save(im.crop((i*512, j*512, (i+1)*512, (j+1)*512)).resize((256,256)), FULL_ZOOM, x+i, y+j)
 
 def main():
     infiles = sys.argv[1:]
